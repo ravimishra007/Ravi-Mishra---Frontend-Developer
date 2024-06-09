9bfae6a7761f0bd60ac778fe67d3fd6492ec3c5f
ravi mishra <ravimishrafea.560722@gmail.com>
2024-06-09 23:15:02 +0530

    avatar section done

--- figma_design/src/App.jsx
+++ figma_design/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Avatar from './components/Avatar'
 import { Header } from './components/Header'
 import { TextSection } from './components/TextSection'
 
@@ -9,6 +10,7 @@ function App() {
     <div className="figma-app">
       <Header/>
       <TextSection/>
+      <Avatar/>
     </div>
 
     </>
